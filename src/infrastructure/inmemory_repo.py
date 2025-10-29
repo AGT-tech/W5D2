65af330d17c5ac9123e75916bc783ae880d6ce11
AGT-tech <abil.teshome@maine.edu>
2025-10-28 20:49:50 -0400

    Update inmemory_repo.py

--- src/infrastructure/inmemory_repo.py
+++ src/infrastructure/inmemory_repo.py
@@ -3,9 +3,11 @@ from src.domain.order import Order
 
 class InMemoryOrderRepository:
   def __init__(self) -> None:
-    self._db: Dict[str, Order] ={}
-  def save((self, order: Order) -> None:
-    self._db(order.id) = order
-
-  def find_by_id(self, id: str) -> Optional[Order]:
-    return self.-db.get(id)
+      self._db: Dict[str, Order] = {}
+  def save(self, order: Order) -> None:
+      self._db(order.id) = order
+  def get_by_id(self, id: str) -> Optional[Order]:
+      return self._db.get(id)
+  def delete(self, id: str) -> None:
+      if id in self._db:
+          del self._db[id]
